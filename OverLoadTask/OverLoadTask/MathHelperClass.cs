15d0999d425b34f847245ef387d777c5b137bdb6
faskaa <fazilnb@code.edu.az>
2023-08-16 13:54:01 +0400

    first commit initizailed

---
+++ OverLoadTask/OverLoadTask/MathHelperClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadTask
+{
+    internal class MathHelperClass
+    {
+        public int PrintValue(int a, int b)
+        {
+            return a + b;
+        }
+        public int PrintValue(bool a)
+        {
+            if (a == true)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public void PrintValue(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public int PrintValue(int a , int b , int c)
+        {
+            return a * b * c;
+        }
+    }
+}
---
+++ OverLoadTask/OverLoadTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OverLoadTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
